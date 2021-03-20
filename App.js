d689bcf139ed6efeeaf9bc7e00945b9313a561e3
clinto92 <clinto92@gmail.com>
2021-03-20 18:26:02 +0530

    TouchableWithoutFeedback and Keyboard.dismiss() method implemented

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Keyboard, StyleSheet, TouchableWithoutFeedback,Keyboard, View } from 'react-native';
 import Header from './components/Header';
 import Content from './components/Content';
 import Pen from './components/Pen';
@@ -10,6 +10,10 @@ export default function App() {
   
   return (
     <React.Fragment>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('Keyboard dismissed');
+    }}>
     <View style={styles.container}>
       <Header /> 
       <Content />
@@ -17,6 +21,7 @@ export default function App() {
       
       <StatusBar style="auto" />
     </View>
+    </TouchableWithoutFeedback>
     </React.Fragment>
   );
 }
